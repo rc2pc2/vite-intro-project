ea177d35cc06b60024a2f7983fc051e42c6b4c5a
rc2pc2 <petriccarcc@gmail.com>
2023-12-13 12:28:47 +0100

    first vite e vue initialization

---
+++ src/components/MainHeader.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+    name: 'MainHeader',
+    data() {
+        return {
+            message: ''
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+        console.warn('Main header initialized!');
+    },
+}
+</script>
+
+<template>
+    <header>
+        <h2>
+            Main Header
+        </h2>
+        <div>
+            <input type="text" v-model="message">
+        </div>
+        <h3>
+            {{ message }}
+        </h3>
+    </header>
+</template>
+
+<style>
+</style>
